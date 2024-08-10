00051db95affd70cf3cd920497f9d83ad20d53ab
Tabitha <123581598+tabithamoon@users.noreply.github.com>
2024-08-10 16:57:49 -0300

    Some sort of home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<title>Tabby's Blog</title>
+
+<div class="flex py-32 h-max w-dvh bg-gray-900 text-white">
+    <div class="flex flex-col lg:flex-row mx-auto">
+        <img class="lg:mr-8 xl:mr-16 w-fit h-[48rem] lg:h-[72rem] xl:h-[80rem]" alt="Me!" src="https://files.tabby.page/chimeraHero.webp"/>
+        <div class="">
+            <p class="text-4xl">Hello,</p>
+            <p class="text-8xl text-red-800">I'm Tabby</p>
+            <p class="text-xl italic text-neutral-500">I'm a robot on the internet</p>
+        </div>
+    </div>
+</div>
